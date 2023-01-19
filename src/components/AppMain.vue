fbe34e2a64581ee94b69116b76ca1a627fa68490
LorenzoTesoro <lorenzotesoro2@gmail.com>
2023-01-19 15:49:16 +0100

    add milestone 3

---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<script>
+
+
+</script>
+<template>
+  <section class="vue-home pt-5">
+    <div class="container">
+      main
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
