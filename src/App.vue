cf1356f96f817fb16efa65ec28142dce81454906
LorenzoTesoro <lorenzotesoro2@gmail.com>
2023-01-19 17:14:28 +0100

    add appmain ajax call

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,12 @@
 import AppNav from './components/AppNav.vue'
 import AppBanner from './components/AppBanner.vue'
 import AppMain from './components/AppMain.vue'
-import axios from 'axios';
 
 export default {
     components: {
         AppNav,
         AppBanner,
         AppMain
-    },
-    data(){
-        return{
-            projects:null,
-            base_api_url: 'http://127.0.0.1:8000',
-            error:null
-        }
-    },
-    methods:{
-        getProjects(url){
-            // ajax call to get all projects
-            axios.get(url).then(response =>{
-                console.log(response.data.results);
-                this.projects = response.data.results;
-            }).catch(error =>{
-                console.log(error.message);
-                this.error = error.message;
-            })
-        }
-    },
-    mounted(){
-        this.getProjects(this.base_api_url + '/api/projects');
     }
 }
 </script>
