4443807adaf3ff3c9b2069c875b707e35eb36fda
LorenzoTesoro <lorenzotesoro2@gmail.com>
2023-01-20 15:49:30 +0100

    add single project view correct

--- src/views/SingleProjectView.vue
+++ src/views/SingleProjectView.vue
@@ -1,25 +1,34 @@
 <script>
-import axios from 'axios'
-import AppBanner from '../components/AppBanner.vue'
+import axios from "axios";
+import AppBanner from "../components/AppBanner.vue";
 
 export default {
-  name: 'SingleProjectView',
+  name: "SingleProjectView",
   components: { AppBanner },
   data() {
     return {
       project: null,
       loading: true,
-      api_base_url: 'http://127.0.0.1:8000'
-    }
+      api_base_url: "http://127.0.0.1:8000",
+    };
+  },
+  methods: {
+    getImagePath(path) {
+      if (path) {
+        return this.base_api_url + "/storage/" + path;
+      }
+      return "/img/placeholder_600.png";
+    },
   },
   mounted() {
-    const url = this.api_base_url + '/api/projects/' + this.$route.params.slug
+    const url = this.api_base_url + "/api/projects/" + this.$route.params.slug;
     console.log(url);
-    axios.get(url)
-      .then(response => {
+    axios
+      .get(url)
+      .then((response) => {
         if (response.data.success) {
-          this.project = response.data.results
-          this.loading = false
+          this.project = response.data.results;
+          this.loading = false;
         } else {
           /* TODO: handle the not found post  
           404 
@@ -27,28 +36,57 @@ export default {
           // https://router.vuejs.org/guide/essentials/navigation.html#navigate-to-a-different-location
         }
         console.log(response);
-      }).catch(error => {
-        console.log(error)
       })
-  }
-}
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 </script>
 
 <template>
   <AppBanner :title="$route.params.slug" />
   <div class="single-project" v-if="project">
-    <img class="img-fluid w-100" :src="api_base_url + '/storage/' + project.cover_image" :alt="project.title">
     <div class="container">
-      <h2>
-        {{ project.title }}
-      </h2>
-      <div class="content">
-        {{ project.description }}
+      <img
+        class="img-fluid"
+        :src="getImagePath(project.cover_image)"
+        :alt="project.title"
+      />
+      <h2>Title: {{ project.title }}</h2>
+      <template v-if="project.description">
+        <strong>Description:</strong>
+        <span>
+          {{ project.description }}
+        </span>
+      </template>
+      <div v-else>
+        <strong>Description:</strong> No description for this project
+      </div>
+      <div class="type">
+        <strong>Type: </strong>
+        <span v-if="project.type">
+          {{ project.type.name }}
+        </span>
+        <span v-else>no types yet</span>
+      </div>
+      <div class="technologies">
+        <strong>Technologies: </strong>
+        <template v-if="project.technologies.length > 0">
+          <span v-for="technology in project.technologies">
+            #{{ technology.name }}
+          </span>
+        </template>
+        <template v-else>
+          <span>No Technologies yet!.</span>
+        </template>
       </div>
     </div>
   </div>
 </template>
 
-
 <style lang="scss" scoped>
+img {
+  max-width: 100px;
+}
 </style>
