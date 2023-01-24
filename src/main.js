c8cb8ffb4db812e49c7dac767eae96cec4b5a295
LorenzoTesoro <lorenzotesoro2@gmail.com>
2023-01-24 11:34:59 +0100

    add logo, fixed footer

--- src/main.js
+++ src/main.js
@@ -5,14 +5,9 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 /* import font awesome icon component */
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 /* import specific icons */
-import {
-  faFacebook,
-  faInstagram,
-  faTwitter,
-  faPinterest,
-} from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 /* add icons to the library */
-library.add(faInstagram, faFacebook, faTwitter, faPinterest);
+library.add(faLinkedin, faGithub);
 /* Import the router  */
 import { router } from "./router";
 createApp(App)
